69bddb6041fc1e7ff3d1884016312e2c8a7c5bde
jungsikjeong <wndtlr1024@gmail.com>
2026-07-05 22:12:57 +0900

    feat: 홈 페이지에서 세션 확인 및 리디렉션 로직 추가

--- apps/auth/web/src/app/(main)/page.tsx
+++ apps/auth/web/src/app/(main)/page.tsx
@@ -1,5 +1,16 @@
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { authClient } from '@/lib/auth-client';
 
-export default function Home() {
-  redirect('/profiles');
+export default async function Home() {
+  const { data: session } = await authClient.getSession({
+    fetchOptions: {
+      headers: { cookie: (await headers()).get('cookie') ?? '' },
+    },
+  });
+
+  if (!session) redirect('/signin');
+
+  // TODO: 계정 account 세팅 페이지, https://myaccount.google.com/ 디자인 참고해서 작업할 예정
+  return;
 }
