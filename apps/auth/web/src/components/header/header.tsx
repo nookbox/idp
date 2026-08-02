76595f2ca68cc67214b07f8c40dbb133ca8275e1
jungsikjeong <wndtlr1024@gmail.com>
2026-08-02 20:46:02 +0900

    feat: 레이아웃 및 헤더 컴포넌트 스타일 수정

--- apps/auth/web/src/components/header/header.tsx
+++ apps/auth/web/src/components/header/header.tsx
@@ -3,18 +3,20 @@ import Link from 'next/link';
 
 export function Header({ children }: { children?: React.ReactNode }) {
   return (
-    <header className="relative z-10 flex items-center justify-between border-b border-white/10 mx-auto w-full max-w-6xl px-8 py-5 xl:px-12">
-      <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="NOOKBOX"
-          width={896}
-          height={224}
-          priority
-          className="h-6 w-auto md:h-8 xl:h-10"
-        />
-      </Link>
-      {children}
+    <header className="relative z-10  border-b border-white/10">
+      <nav className="max-w-6xl w-full mx-auto px-8 py-5 xl:px-12 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="NOOKBOX"
+            width={896}
+            height={224}
+            priority
+            className="h-6 w-auto md:h-8 xl:h-10"
+          />
+        </Link>
+        {children}
+      </nav>
     </header>
   );
 }
